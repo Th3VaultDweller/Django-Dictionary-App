6f4b138336bd665b97c598c7e22acb128296e3d2
Th3VaultDweller <aztec242@yandex.ru>
2023-09-20 15:41:51 +0300

    Updated the functions

--- mydictionary/englishdictionary/views.py
+++ mydictionary/englishdictionary/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
-
+from PyDictionary import PyDictionary
 # Create your views here.
+
+def base(request):
+    "Репрезентация начальной страницы словаря"
+    return render(request, 'base.html')
+
+def word_info(request):
+    "Репрезентация страницы с информацией по искомому слову"
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    synonyms = dictionary.synonym(search)
+    antonyms = dictionary.antonym(search)
+
+    context = {
+        'meaning': meaning, 
+        'synonyms': synonyms,
+        'antonyms': antonyms
+    }
+
+    return render(request, 'word_info.html', context)
