34a096d76e349352106b5fbe35148c019e568d05
Th3VaultDweller <aztec242@yandex.ru>
2023-09-20 17:35:31 +0300

    Minor Fixes

--- mydictionary/englishdictionary/views.py
+++ mydictionary/englishdictionary/views.py
@@ -2,23 +2,30 @@ from django.shortcuts import render
 from PyDictionary import PyDictionary
 # Create your views here.
 
-
 def base(request):
     "Репрезентация начальной страницы словаря"
     return render(request, 'base.html')
 
 def word_info(request):
     "Репрезентация страницы с информацией по искомому слову"
-    search = request.GET.get('search')
     dictionary = PyDictionary()
-    meaning = dictionary.meaning(search)
-    antonyms = dictionary.antonym(search)
-    synonyms = dictionary.synonym(search)
+    if request.method == 'POST':
+        word = request.POST['word']
+        meaning = dictionary.meaning(word)
+        antonyms = dictionary.antonym(word)
+        synonyms = dictionary.synonym(word)
 
-    context = {
-        'meaning': meaning['Noun'][0],
-        'synonyms': synonyms,
-        'antonyms': antonyms,
-    }
+        # context = {
+        #     'meaning': meaning['Noun'][0],
+        #     'synonym': synonyms,
+        #     'antonym': antonyms,
+        # }
 
-    return render(request, 'word_info.html', context)
+        return render(request, 'word_info.html', 
+                      {'word': word, 
+                       'meaning': meaning, 
+                       'antonym': antonyms, 
+                       'synonym': synonyms }
+                       )
+    
+    return render(request, 'word_info.html')
