55e6063a2a45b87b48c17c17f24efd004dcbd313
Th3VaultDweller <aztec242@yandex.ru>
2023-09-21 22:05:04 +0300

    Formatting test

--- mydictionary/englishdictionary/views.py
+++ mydictionary/englishdictionary/views.py
@@ -1,28 +1,36 @@
 from django.shortcuts import render
+
 # from PyDictionary import PyDictionary
 from PyMultiDictionary import MultiDictionary
+
 # Create your views here.
+
 
 def base(request):
     "Репрезентация начальной страницы словаря"
-    return render(request, 'base.html')
+    return render(request, "base.html")
+
 
 def word_info(request):
     "Репрезентация страницы с информацией по искомому слову"
     dictionary = MultiDictionary()
-    if request.method == 'POST':
-        word = request.POST['word']
-        meaning = dictionary.meaning('en', word)
-        antonyms = dictionary.antonym('en', word)
-        synonyms = dictionary.synonym('en', word)
-        translation = dictionary.translate('en', word, to='ru')
+    if request.method == "POST":
+        word = request.POST["word"]
+        meaning = dictionary.meaning("en", word)
+        antonyms = dictionary.antonym("en", word)
+        synonyms = dictionary.synonym("en", word)
+        translation = dictionary.translate("en", word, to="ru")
 
-        return render(request, 'word_info.html', 
-                {'word': word, 
-                'meaning': meaning, 
-                'antonyms': antonyms, 
-                'synonyms': synonyms,
-                'translation': translation}
-                )
-    
-    return render(request, 'word_info.html')
+        return render(
+            request,
+            "word_info.html",
+            {
+                "word": word,
+                "meaning": meaning,
+                "antonyms": antonyms,
+                "synonyms": synonyms,
+                "translation": translation,
+            },
+        )
+
+    return render(request, "word_info.html")
